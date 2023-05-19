0f877d9140ecb0f609f16f7a2dcce4d8c9396127
mathmagician001 <133428327+mathmagician001@users.noreply.github.com>
2023-05-20 00:21:50 +0400

    Create index.js

---
+++ homeworks/#2/index.js
@@ -0,0 +1,65 @@
+//1
+const removeFirstElem = (arr, index = 0, arrClone = [...arr]) => {
+    if(arrClone.length <= 1) {
+        return [];
+    }
+    
+    if(index === arrClone.length - 1) {
+        return arrClone.slice(0,-1);
+    }
+
+    arrClone[index] = arrClone[index+1];
+
+    return removeFirstElem(arrClone, index + 1, arrClone);
+}
+
+
+
+//2
+const customFlat = arr => {
+    let result = [];
+
+    if(!Array.isArray(arr)) {
+        return arr;
+    }
+
+    for(let i = 0; i < arr.length; i++) {
+        result = result.concat(customFlat(arr[i]));
+    }
+
+    return result;
+}
+
+//3
+const sumOfDigits = num => {
+    let sum = 0;
+
+    if(num < 0) {
+        num = Math.abs(num);
+    }
+    
+    while(num) {
+        sum += num % 10;
+        num = Math.trunc(num / 10);
+    }
+    
+    if(sum > 9) {
+        return sumOfDigits(sum);    
+    }
+    
+    return sum;
+}
+
+//4
+const swapKeyValues = obj => {
+    const result = {};
+    for(let [key, value] of Object.entries(obj)) {
+        if(result.hasOwnProperty(value)) {
+            result[value] = [...result[value], key]
+        } else {
+            result[value] = key;            
+        }
+    }
+
+    return result;
+}
