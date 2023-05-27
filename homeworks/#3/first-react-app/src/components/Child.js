09dcf347499f02d3a192db94a2a8e31177e2914b
mathmagician001 <133428327+mathmagician001@users.noreply.github.com>
2023-05-27 15:19:37 +0400

    Delete homeworks/#3/first-react-app directory

--- homeworks/#3/first-react-app/src/components/Child.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-function Child(props) {
-  const { inputValue, handleInputChange } = props;
-  return <input type="text" value={inputValue} onChange={handleInputChange} />;
-}
-
-export default Child;
