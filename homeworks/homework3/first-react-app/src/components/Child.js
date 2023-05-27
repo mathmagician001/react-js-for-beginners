c17c30dc0f45ae4b228ede9a5c8e013fa6c3e4cb
mathmagician001 <133428327+mathmagician001@users.noreply.github.com>
2023-05-27 15:21:54 +0400

    Update Child.js

--- homeworks/homework3/first-react-app/src/components/Child.js
+++ homeworks/homework3/first-react-app/src/components/Child.js
@@ -2,6 +2,7 @@ import React from "react";
 
 function Child(props) {
   const { inputValue, handleInputChange } = props;
+
   return <input type="text" value={inputValue} onChange={handleInputChange} />;
 }
 
